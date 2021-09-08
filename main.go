beb7f514cb1e2947cb29ec6836779501e41e627b
alexandreLamarre <alex7285@gmail.com>
2021-09-08 18:30:28 -0400

    different ways to start goroutines

--- main.go
+++ main.go
@@ -2,8 +2,36 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
+
+func somePrint(s string) {
+	for i := 0; i < 3; i++ {
+		fmt.Println(s)
+		time.Sleep(1 * time.Millisecond)
+	}
+}
 
 func main() {
 	fmt.Println("Hello world~!")
+	//direct call
+	somePrint("direct call")
+
+	//goroutine function call
+	go somePrint("goroutine-1")
+
+	//goroutine with anonymous function
+	go func() {
+		somePrint("goroutine-2")
+	}()
+
+	//goroutine with function value call
+	fv := somePrint
+	go fv("goroutine-3")
+
+	// wait for goroutines to end
+	fmt.Println("Waiting for goroutines...")
+	time.Sleep(time.Millisecond * 100)
+
+	fmt.Println("Done.")
 }
